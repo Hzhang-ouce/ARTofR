593cd008362fecc88caa74897ba4874d85b76125
Hzhang-ouce <54635941+Hzhang-ouce@users.noreply.github.com>
2021-09-18 21:23:08 +0100

    add error message

--- R/xxx_divider.R
+++ R/xxx_divider.R
@@ -65,8 +65,11 @@ xxx_divider1<-function(mystring=NULL){
   mid <- character(nt)
   hashes = 65
   blanks <- (hashes - leftSideHashes - rightSideHashes - n0)
+  if (!length(blanks)==1) {
+    stop('you must give me a sentence less than 65 characters')
+  }
   if (blanks<0) {
-    stop('you must give me a line less than 65 characters')
+    stop('you must give me a sentence less than 65 characters')
   }
   blanks1 = round(blanks/2)
   blanks2 <- blanks - blanks1
